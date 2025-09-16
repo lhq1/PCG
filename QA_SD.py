fee7c594f30dda6118dc2e3c551d471d7fc16a8f
Liu Hongqing <33518873+lhq1@users.noreply.github.com>
2025-09-16 14:19:26 +0800

    Update QA_SD.py

--- QA_SD.py
+++ QA_SD.py
@@ -9,7 +9,7 @@ def seed_size(c, n, t, security_parameter=128,field_size=128, d=2):
 
 if __name__=="__main__":
     c_set = [2, 3, 4]
-    n_set = [20, 25, 30]
+    n_set = [20, 25]
     secpar_set = [80, 128]
     t_set = [[33, 57],
              [19, 24],
@@ -23,5 +23,5 @@ if __name__=="__main__":
                 n = n_set[k]
                 print("c={}, n={}, t={}, security paramter={}".format(c, n, t, security_parameter))
                 size = seed_size(c,n,t, security_parameter)
-                expand = math.pow(2, n-size)
+                expand = math.pow(2, n-size+1)
                 print("seed size is 2^{}, expand is {}".format(size, expand))
